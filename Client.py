c0fa2508998a8c62a5f80dcbc417af151d832286
shutebt01 <shutebt01@users.noreply.github.com>
2015-02-22 12:37:00 +0000

    Update Client.py
    Added PM

--- Client.py
+++ Client.py
@@ -4,6 +4,11 @@ Created on 12 Feb 2015
 @author: shutebt01
 '''
 
+'''
+Packet formating:
+    [type, src-name, src-group, data]
+'''
+
 import socket, threading, json
 
 name = input("Enter User Name: ")
@@ -22,10 +27,20 @@ class InputThread(threading.Thread):
         while True:
             inp = input()
             data = None
-            if not(inp.startswith("!")):
+            if not(inp.startswith('!')):
                 data = json.dumps(["Message", name, room, inp])
             else:
-                pass
+                # Creates initial packet with data for tracking
+                packet = ["Event", name, room]
+                if inp.startswith("!pm"):
+                    split = inp.split(' ', 1)
+                    #TODO implement better validation
+                    if (len(split) == 2):
+                        #Adds data to packet
+                        packet[3] = "pm"
+                        packet[4] = split[0]
+                        packet[5] = split[1]
+                data = json.dumps(packet)
             s.send(data.encode("ascii"))
 
 class OutputThread(threading.Thread):
@@ -38,6 +53,9 @@ class OutputThread(threading.Thread):
             array = json.loads(data)
             if array[0] == "Message":
                 print(array[1] + " (" + array[2] + "):" + array[3])
+            elif array[0] == "Event":
+                if array[3] == "pm":
+                    print(array[1] + " (" + array[2] + ") -> You" + array[5])
             
 Inp = InputThread()
 Inp.start()
